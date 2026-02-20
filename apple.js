2f97d85a42c8884dec721bc93babeb7fe5043c3a
anantharaj20 <149312516+anantharaj20@users.noreply.github.com>
2026-02-20 13:46:36 +0530

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,20 @@
+/*let scrollcontainer=document.querySelector(".gallery");
+let back=document.getElementById("back");
+let forward=document.getElementById("forward");
+  
+scrollcontainer.addEventListener("wheel",(evt)=>{
+    evt.preventDefault();
+    scrollcontainer.scrollLeft +=evt.deltaY;
+    scrollcontainer.style.scrollBehavior="auto"
+});
+
+forward.addEventListener("click",()=>{
+    scrollcontainer.style.scrollBehavior="smooth"
+    scrollcontainer.scrollLeft += 900;
+});
+
+back.addEventListener("click",()=>{
+    scrollcontainer.style.scrollBehavior="smooth"
+    scrollcontainer.scrollLeft -=900;
+});
+*/
